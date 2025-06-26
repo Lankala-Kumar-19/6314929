6e32d4c10d04106acbce187e79d54fd8ddf588dd
Lankala Durga Prasanna Kumar <lankalakumar08@gmail.com>
2025-06-26 09:05:50 +0530

    Add files via upload

---
+++ Java_Deepskilling/Week2/TDD using Mockito/Exercise1/MyServiceTest.java
@@ -0,0 +1,28 @@
+package mocktitoExercise;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+
+
+@ExtendWith(MockitoExtension.class)
+class MyServiceTest {
+	@Mock
+	ExternalApi mockApi;
+	
+	
+
+	@Test
+	void testExternalApi() {
+		when(mockApi.getData()).thenReturn("Mock API");
+		MyService service=new MyService(mockApi);
+		assertEquals("Mock API",service.fetchData());
+	}
+
+}
