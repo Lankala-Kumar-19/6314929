69deb81ee9bb53546777b58ebcf141210679f201
KUMAR <KUMAR@Kumar>
2025-07-02 14:45:37 +0530

    Revert "spring-data-jpa-handson-2 done"
    This reverts commit 0ef4abe3adec1cd2e81810e0e31c144620383ec8.

--- Java_Deepskilling/Week3/Flipkart/src/main/java/com/example/Flipkart/Repos/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Flipkart.Repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Flipkart.Entities.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long>{
-
-}
